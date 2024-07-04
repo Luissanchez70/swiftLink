1fbb80d7624344aa60da2ea34e18e71340fee981
Luis <luis.fernandomunoz@plexus.es>
2024-07-04 08:39:17 +0200

    Initial Commit

---
+++ swiftLink/swiftLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftLinkApp.swift
+//  swiftLink
+//
+//  Created by Luis Fernando Sanchez Muñoz on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
